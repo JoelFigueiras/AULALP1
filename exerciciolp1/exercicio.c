e721499a9e2807ec2acca18c01c22f431c43e11a
Joel <joelfigueiras@live.com.pt>
2020-11-03 19:06:53 +0000

    main

---
+++ exerciciolp1/exercicio.c
@@ -0,0 +1,46 @@
+/**
+ * @file exercicio.c
+ * @author Joel Figueiras (joelfigueiras@live.com.pt)
+ * @brief 
+ * @version 0.1
+ * @date 2020-11-03
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+
+int a, b, soma, divisao, subetracao, multiplicacao;
+
+printf("Meta o numero: ");
+scanf("%d", &a);
+
+printf("meta o numero: ");
+scanf("%d", &b);
+
+soma = a + b;
+
+printf(" a soma e: %d ", soma);
+
+// divisao
+
+printf("meta o numero: %d ", a);
+printf("meta o numero: %d", b);
+
+divisao = a/b;
+
+printf("a divisao e: %d", divisao);
+
+
+
+
+
+
+
+
+
+return 0;
+}
